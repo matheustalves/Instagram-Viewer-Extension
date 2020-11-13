9fc5be224ee83a91731de426179cf76748b1b740
Matheus Teixeira <43442292+matheustalves@users.noreply.github.com>
2020-11-13 15:25:59 -0300

    updated run.js
    fixed bottom login overlay and stories div

--- run.js
+++ run.js
@@ -1,36 +1,50 @@
 var style = document.createElement("style");
-            document.head.appendChild(style);
-            style.sheet.insertRule(".RnEpo._Yhr4 { display: none ;}", 0);
-            style.sheet.insertRule(".vCZ2N { display: none ;}", 0);
-            style.sheet.insertRule(" body { overflow: auto !important ;}", 0);
+document.head.appendChild(style);
+style.sheet.insertRule(".RnEpo._Yhr4 { display: none ;}", 0);
+style.sheet.insertRule("._4EzTm { display: none ;}", 0);
+style.sheet.insertRule(" body { overflow: auto !important ;}", 0);
+style.sheet.insertRule("._4bSq7 {display: none ;}", 0);
 
+var btn = document.querySelector(
+  "#react-root > section > main > div > div._2z6nI > div.Igw0E.rBNOH.eGOV_._4EzTm > div > button"
+);
+
+if (btn !== null) {
+  btn.click();
+}
 
 // find newly added links
-var obs=new MutationObserver(function(muts){
+var obs = new MutationObserver(function (muts) {
   for (let mut of muts) {
     for (let n of mut.addedNodes) {
-      if (n.classList.contains('Nnq7C')) fixLinks(n);
-      }
+      if (n.classList.contains("Nnq7C")) fixLinks(n);
     }
-  });
+  }
+});
 
-obs.observe(document.body, {attributes: false, childList: true, subtree: true});
+obs.observe(document.body, {
+  attributes: false,
+  childList: true,
+  subtree: true,
+});
 
 function openLink(ev) {
-  this.cloneNode(false).dispatchEvent(new MouseEvent('click',ev));
+  this.cloneNode(false).dispatchEvent(new MouseEvent("click", ev));
 }
 
 function fixLinks(r) {
-  (r || document.body).querySelectorAll(':scope a[href^="/"]').forEach(function(e){
-    if (!e.sclk) {
-      e.sclk=true;
-      e.addEventListener('click',openLink);
+  (r || document.body)
+    .querySelectorAll(':scope a[href^="/"]')
+    .forEach(function (e) {
+      if (!e.sclk) {
+        e.sclk = true;
+        e.addEventListener("click", openLink);
       }
     });
-  }
+}
 
 // fix links already on page
-setTimeout(fixLinks,2000);
+setTimeout(fixLinks, 2000);
 // page lazy to load?
-setTimeout(fixLinks,10000);
-setTimeout(fixLinks,20000);
+setTimeout(fixLinks, 10000);
+setTimeout(fixLinks, 20000);
